fbb76e4df51e93b6ab575f7b7827df960012cafb
Koji <gyakusu@gmail.com>
2024-04-19 17:00:20 +0900

    main関数の呼び出し方を変えました（サンプルはコメントにあります．）．保持器のVTKを出力するようにしました．

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,30 @@ use na::Vector3;
 use std::f32::consts::PI;
 
 use mophing::vtk::io;
-use mophing::vtk::brg::Brg;
-use mophing::vtk::brg::{CageParameter, PocketParameter, NeckParameter};
+use mophing::vtk::brg::{Brg, CageParameter, PocketParameter, NeckParameter};
 
-// example: `cargo run --release -- data/Tetra_linspace.vtu 2.345e-3 2.850e-3 0.93e-3 2.10e-3 0.10e-3 0.825e-3 1.70e-3 1.20e-3 2.45e-3 0.152e-3 100`
+// example: `cargo run --release "data/Tetra.vtu,data/face_and_edge_index.xml,data/Tetra_linspace.vtu" "2.345e-3,2.850e-3,0.93e-3,2.10e-3,0.10e-3,0.825e-3,1.70e-3,1.20e-3,2.45e-3,0.152e-3" "100"`
 fn main() {
 
     let args: Vec<String> = std::env::args().collect();
-    let get_arg = |i: usize| args[i].parse::<f32>().unwrap();
+    let parsed_args: Vec<Vec<String>> = args[1..]
+        .iter()
+        .map(|arg| {
+            arg.split(',')
+                .map(|s| s.to_string())
+                .collect()
+        })
+        .collect();
 
-    println!("Argument:");
-    println!("{}", args.join(" "));
+    let get_str = |i: usize| parsed_args[0][i].as_str();
+    let get_f32 = |i: usize| parsed_args[1][i].parse::<f32>().unwrap();
+    let get_usize = |i: usize| parsed_args[2][i].parse::<usize>().unwrap();
 
-    let origin_path = "data/Tetra.vtu";
-    let index_path = "data/face_and_edge_index.xml";
-    let write_path = args[1].as_str();
+    println!("{:?}", parsed_args);
+
+    let origin_path = get_str(0);
+    let index_path = get_str(1);
+    let write_path = get_str(2);
 
     let mesh = io::read_vtk(origin_path);
 
@@ -25,20 +34,20 @@ fn main() {
         axis: Vector3::new(0.0, 0.0, 1.0),
         theta0: -PI / 6.0,
         theta1:  PI / 6.0,
-        r0: get_arg(2),
-        r1: get_arg(3),
-        h0: get_arg(4),
-        h1: get_arg(5),
-        bevel: get_arg(6),
+        r0: get_f32(0),
+        r1: get_f32(1),
+        h0: get_f32(2),
+        h1: get_f32(3),
+        bevel: get_f32(4),
         pocket: PocketParameter {
             x: Vector3::new(0.0, 2.65e-3, 2.00e-3),
-            r: get_arg(7),
+            r: get_f32(5),
         },
         neck: NeckParameter {
-            x: Vector3::new(0.0, 2.65e-3, get_arg(8)),
-            r: get_arg(9),
-            h: get_arg(10),
-            dh: get_arg(11),
+            x: Vector3::new(0.0, 2.65e-3, get_f32(6)),
+            r: get_f32(7),
+            h: get_f32(8),
+            dh: get_f32(9),
             h_ratio: 0.355,
             r_ratio: 0.55,
         },
@@ -57,7 +66,8 @@ fn main() {
 
     brg.project_all();
 
-    let iteration = args[12].parse::<usize>().unwrap();
+    let iteration = get_usize(0);
+    
     for _ in 0..iteration {
         brg.smooth_face();
     }
