b0180f64845dd749d968bbafcc7d564e4ef3eaa5
Koji <gyakusu@gmail.com>
2024-06-13 12:42:41 +0900

    `mophing`を`morphing`に変えたらプロジェクト破綻の危機！でしたがCargo.tomlの設定を変えなんとかなりました．ビルド，テスト実行，成功を確認しています．

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 pub mod vtk;
-use morphing::vtk::brg::{Brg, CageParameter, };
 
-use pyo3::prelude::*;
-use CageParameter::*;
+// use pyo3::prelude::*;
+// use CageParameter::*;
 
-#[pymodule]
-fn morphing(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<CageParameter>()?;
-    Ok(())
-}
+// #[pymodule]
+// fn morphing(_py: Python, m: &PyModule) -> PyResult<()> {
+//     m.add_class::<CageParameter>()?;
+//     Ok(())
+// }
---
+++ src/morphing.rs
@@ -0,0 +1,21 @@
+// // morphing.rs
+
+// use crate::vtk::io;
+// use crate::vtk::brg::{Brg, CageParameter, };
+
+
+// /Morphing
+//     src/
+//         main.rs
+//         lib.rs
+//         morphing.rs
+//         vtk/
+//             io.rs
+//             brg.rs
+//             mesh.rs
+//             face.rs
+//             point.rs
+//             tetra.rs
+//             flower.rs
+//             math.rs
+
